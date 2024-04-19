abdd48512a4512cf234cfdbed738e90cac663ad3
Nika <nikolozberoshvili22@gmail.com>
2024-04-19 09:38:01 +0000

    ver_movaswari_chedavda

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,38 @@
-import React, { useContext } from 'react'
+import React, {useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { AppContext } from '../App'
+import first_array from "./routes/Array_1.json";
+import second_array from "./routes/Array_2.json";
+import List from "./List";
+
 
 export const Home = () => {
   const { state, setState } = useContext(AppContext)
+
+  const [combinedArray, setCombinedArray] = useState([]);
+
+  const combineArrays = () => {
+    setCombinedArray([...first_array, ...second_array]);
+  }
+ 
+
   return (
     <div className='App'>
       <header className='App-header'>
         <Link to='list'>{state}</Link>
+        <button onClick={combineArrays}>Combine two arrays</button>
+        <div>
+          {
+            JSON.parse(combinedArray).map((item, index) => {
+              return (
+              <List 
+               key={item.id.toString()} 
+               itemName={item.name} 
+               itemBody={item.body}/>
+              )
+            })
+          }
+        </div>
       </header>
     </div>
   )
--- src/routes/List.jsx
+++ src/routes/List.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
-export default function List() {
-  return <div>list</div>
+export default function List({ itemName, itemBody }) {
+  return (
+    <div>
+      <h2>{itemName}</h2>
+      <p>{itemBody}</p>
+    </div>)
 }
